a8218bbd99c3bc8ee9f42d6bfbf6e85dbaad673a
uomma <marcelloleone975@gmail.com>
2023-05-31 01:06:17 +0200

    milestone 2

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -1,13 +1,18 @@
 <?php
 
 namespace App\Http\Controllers\Api;
-
+use App\Models\Post;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
+
+
     public function index() {
-        return ['check1'];
+
+        $posts = Post::all();
+
+        return response()->json(['done' => true, 'check'=> $posts]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -20,5 +20,5 @@ use Illuminate\Support\Facades\Route;
 });
  */
 
- Route::get('test', [ProjectController::class, 'index']);
+ Route::get('posts', [ProjectController::class, 'index']);
 
